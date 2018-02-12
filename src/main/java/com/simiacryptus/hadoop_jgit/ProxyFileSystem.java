6c8480f679440900e61a443a5117cef67c010dff
Andrew Charneski <acharneski@gmail.com>
2018-02-12 06:15:01 -0800

    ``

---
+++ src/main/java/com/simiacryptus/hadoop_jgit/ProxyFileSystem.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2018 by Andrew Charneski.
+ *
+ * The author licenses this file to you under the
+ * Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.simiacryptus.hadoop_jgit;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.util.Progressable;
+
+import java.io.IOException;
+
+public abstract class ProxyFileSystem extends org.apache.hadoop.fs.FileSystem implements AutoCloseable {
+  private Path workingDirectory;
+  
+  protected abstract GitRepoFileSystem route(Path f);
+  
+  protected abstract Path filter(Path f);
+  
+  @Override
+  public FSDataInputStream open(final Path f, final int bufferSize) throws IOException {
+    return route(f).open(filter(f), bufferSize);
+  }
+  
+  @Override
+  public FileStatus[] listStatus(final Path f) throws IOException {
+    return route(f).listStatus(filter(f));
+  }
+  
+  @Override
+  public Path getWorkingDirectory() {
+    return workingDirectory;
+  }
+  
+  @Override
+  public void setWorkingDirectory(final Path new_dir) {
+    this.workingDirectory = new_dir;
+  }
+  
+  @Override
+  public FileStatus getFileStatus(final Path f) throws IOException {
+    return route(f).getFileStatus(filter(f));
+  }
+  
+  @Override
+  public FSDataOutputStream create(final Path f, final FsPermission permission, final boolean overwrite, final int bufferSize, final short replication, final long blockSize, final Progressable progress) throws IOException {
+    return route(f).create(filter(f), permission, overwrite, bufferSize, replication, blockSize, progress);
+  }
+  
+  @Override
+  public FSDataOutputStream append(final Path f, final int bufferSize, final Progressable progress) throws IOException {
+    return route(f).append(filter(f), bufferSize, progress);
+  }
+  
+  @Override
+  public boolean rename(final Path src, final Path dst) throws IOException {
+    return route(src).rename(filter(src), dst);
+  }
+  
+  @Override
+  public boolean delete(final Path f, final boolean recursive) throws IOException {
+    return route(f).delete(filter(f), recursive);
+  }
+  
+  @Override
+  public boolean mkdirs(final Path f, final FsPermission permission) throws IOException {
+    return route(f).mkdirs(filter(f), permission);
+  }
+}
