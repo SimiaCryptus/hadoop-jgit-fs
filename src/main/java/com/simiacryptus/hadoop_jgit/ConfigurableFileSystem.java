df00da0146711b26ae66e952c4a27a84a2d65752
Andrew Charneski <acharneski@gmail.com>
2018-02-12 18:13:47 -0800

    ``

---
+++ src/main/java/com/simiacryptus/hadoop_jgit/ConfigurableFileSystem.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 by Andrew Charneski.
+ *
+ * The author licenses this file to you under the
+ * Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.simiacryptus.hadoop_jgit;
+
+import java.util.function.Supplier;
+
+public abstract class ConfigurableFileSystem extends org.apache.hadoop.fs.FileSystem implements AutoCloseable {
+  protected String getProperty(final String key) {
+    return getProperty(key, () -> {
+      throw new RuntimeException("No config found for " + key);
+    });
+  }
+  
+  protected String getProperty(final String key, String defaultValue) {
+    return getProperty(key, () -> defaultValue);
+  }
+  
+  protected String getProperty(final String key, Supplier<String> defaultValue) {
+    String hadoopValue = getConf().get(key);
+    String javaValue = System.getProperty(key);
+    if (null == hadoopValue && null == javaValue) return defaultValue.get();
+    if (null != hadoopValue && null != javaValue && !javaValue.equals(hadoopValue))
+      throw new RuntimeException("Conflicting configs for " + key);
+    return null == hadoopValue ? javaValue : hadoopValue;
+  }
+}
