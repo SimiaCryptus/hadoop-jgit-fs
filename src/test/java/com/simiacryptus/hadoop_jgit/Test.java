b1ded37be331a2dc6d80d3a765fdf770e8c392e2
Andrew Charneski <acharneski@gmail.com>
2020-01-05 23:05:05 -0800

    Begin implementation of verify/update

--- src/test/java/com/simiacryptus/hadoop_jgit/Test.java
+++ src/test/java/com/simiacryptus/hadoop_jgit/Test.java
@@ -33,7 +33,7 @@ public class Test {
 
 
   public static void main(String[] args) {
-    //System.setProperty("fs.jgit.auth.user")
+    //com.simiacryptus.ref.wrappers.RefSystem.setProperty("fs.jgit.auth.user")
     String pathString = args.length == 0 ? "https://git-codecommit.us-west-2.amazonaws.com/v1/repos/simiacryptus-test:/master/README.md" : args[0];
     //String pathString = args.length == 0 ? "git://github.com/SimiaCryptus/hadoop-jgit-fs.git/acharneski-patch-1/README.md" : args[0];
     try {
